49347ac5fc97698a5336dd372cf7ecae1f17b55a
Hiroyuki Ikezoe <ikezoe@clear-code.com>
2010-05-24 15:45:13 +0900

    load png data as binary.

--- test/test-utils.c
+++ test/test-utils.c
@@ -2,6 +2,12 @@
 #include <cutter.h>
 #include <string.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
 #include "mz-utils.h"
 
 void test_get_content_type (void);
@@ -88,24 +94,77 @@ test_get_attachment_body_place (void)
     cut_assert_equal_substring(expected_body, body, size);
 }
 
+static char *
+load_data (const char *path, unsigned int *size)
+{
+    char *data = NULL;
+    char *data_path;
+    struct stat stat_buf;
+    int fd;
+    unsigned int bytes_read;
+
+    data_path = cut_build_fixture_data_path(path, NULL);
+    cut_take_string(data_path);
+
+    fd = open(data_path, O_RDONLY);
+    if (fd < 0)
+        return NULL;
+
+    if (fstat(fd, &stat_buf) < 0)
+        goto error;
+
+    if (stat_buf.st_size <= 0 || !S_ISREG (stat_buf.st_mode))
+        goto error;
+
+    data = malloc(stat_buf.st_size);
+    if (!data)
+        goto error;
+
+    *size = stat_buf.st_size;
+    while (bytes_read < *size) {
+        int rc;
+
+        rc = read(fd, data + bytes_read, *size - bytes_read);
+        if (rc < 0) {
+            if (errno != EINTR)
+                goto error;
+        } else if (rc == 0) {
+            break;
+        } else {
+            bytes_read += rc;
+        }
+    }
+
+    close(fd);
+    return data;
+
+error:
+    free(data);
+    close(fd);
+
+    return NULL;
+}
+
 void
 test_get_decoded_attachment_body (void)
 {
     const char *content;
-    const char *expected;
+    char *expected;
     const char *body;
     unsigned int size = 0;
+    unsigned int expected_size = 0;
 
     content = cut_get_fixture_data_string("attachment", NULL);
     cut_assert_not_null(content);
 
-    expected = cut_get_fixture_data_string("t.png", NULL);
+    expected = load_data("t.png", &expected_size);
+    cut_take_string(expected);
     cut_assert_not_null(expected);
 
     body = mz_utils_get_decoded_attachment_body(content, &size);
     cut_assert_not_null(body);
     cut_assert_not_equal_int(0, size);
 
-    cut_assert_equal_int(strlen(expected), size);
+    cut_assert_equal_int(expected_size, size);
     cut_assert_equal_substring(expected, body, size);
 }
