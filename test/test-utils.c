a00d0041d2ccfe56f9d74a9cd73919394fd3b1f5
Hiroyuki Ikezoe <ikezoe@clear-code.com>
2010-05-28 13:27:02 +0900

    Use ISO-2022-JP encoding.

--- test/test-utils.c
+++ test/test-utils.c
@@ -161,7 +161,8 @@ test_get_content_disposition_mime_encoded_filename (void)
 
     cut_assert_true(mz_utils_get_content_disposition(content, length, &type, &filename));
     cut_assert_equal_string("attachment", type);
-    cut_assert_equal_string("日本語.txt", filename);
+    cut_assert_equal_string("\x1B\x24\x42\x46\x7C\x4B\x5C\x38\x6C\x1B\x28\x42\x2e\x74\x78\x74", /* 日本語.txt */
+                            filename);
 }
 
 void
@@ -179,7 +180,10 @@ test_get_rfc2311_filename (void)
 
     cut_assert_true(mz_utils_get_content_disposition(content, length, &type, &filename));
     cut_assert_equal_string("attachment", type);
-    cut_assert_equal_string("新しいテキスト ドキュメント.txt", filename);
+    cut_assert_equal_string("\x1B\x24\x42\x3F\x37\x24\x37\x24\x24\x25\x46\x25\x2D\x25" /* 新しいテキスト　ドキュメント.txt */
+                            "\x39\x25\x48\x1B\x28\x42\x20\x1B\x24\x42\x25\x49\x25\x2D"
+                            "\x25\x65\x25\x61\x25\x73\x25\x48\x1B\x28\x42\x2E\x74\x78\x74",
+                            filename);
 }
 
 void
