50708cdc5790133e70f5adaf2e656b044626f740
Hiroyuki Ikezoe <ikezoe@clear-code.com>
2010-06-07 13:13:24 +0900

    check the return variable of deflateInit2.

--- test/test-zlib.c
+++ test/test-zlib.c
@@ -19,16 +19,24 @@ teardown (void)
     deflateEnd(&zlib_stream);
 }
 
+static void
+assert_z_ok (int zlib_code)
+{
+    cut_assert_equal_int(Z_OK, zlib_code);
+}
+
 void
 test_init (void)
 {
     int compression_level = 1;
+    int ret;
 
-    deflateInit2(&zlib_stream,
-                 compression_level,
-                 Z_DEFLATED,
-                 -8, /* windowBits ? */
-                 9, /* Use maximum memory for optimal speed.*/
-                 Z_DEFAULT_STRATEGY);
+    ret = deflateInit2(&zlib_stream,
+                       compression_level,
+                       Z_DEFLATED,
+                       -8, /* windowBits ? */
+                       9, /* Use maximum memory for optimal speed.*/
+                       Z_DEFAULT_STRATEGY);
+    assert_z_ok(ret);
 }
 
