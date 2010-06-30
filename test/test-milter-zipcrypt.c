d07fcbf36f2330e48c05d2849dea9280cb6ca6be
Hiroyuki Ikezoe <ikezoe@clear-code.com>
2010-07-01 08:53:44 +0900

    Use unzip to check the attachment file.

--- test/test-milter-zipcrypt.c
+++ test/test-milter-zipcrypt.c
@@ -17,11 +17,13 @@ void test_no_attachment (void);
 
 static GCutProcess *test_server;
 static GCutProcess *milter_zipcrypt;
+static GCutProcess *unzip;
 static GString *modified_message;
 
-#define CONNECTION_SPEC "unix:test-milter-zipcrypt.sock"
 static char *temporary_socket;
 static MzList *modified_attachments;
+static int attachment_fd;
+static char *attachment_file;
 
 static void
 run_milter_zipcrypt (void)
@@ -45,11 +47,11 @@ run_milter_zipcrypt (void)
 static void
 create_temporary_socket (void)
 {
-    int fd;
+    int temporary_fd;
 
     temporary_socket = g_strdup("unix:milter-zipcrypt-socket-XXXXXX");
-    fd = mkstemp(temporary_socket);
-    close(fd);
+    temporary_fd = mkstemp(temporary_socket);
+    close(temporary_fd);
     cut_remove_path(temporary_socket, NULL);
 }
 
@@ -57,10 +59,14 @@ void
 setup (void)
 {
     test_server = NULL;
+    unzip = NULL;
     modified_attachments = NULL;
+    attachment_fd = -1;
 
+    attachment_file = strdup("MilterZipCryptTestXXXXXX");
     create_temporary_socket();
     cut_trace(run_milter_zipcrypt());
+    cut_make_directory("tmp", NULL);
 }
 
 static void
@@ -85,8 +91,16 @@ teardown (void)
         g_object_unref(milter_zipcrypt);
     if (test_server)
         g_object_unref(test_server);
+    if (unzip)
+        g_object_unref(unzip);
     g_free(temporary_socket);
     mz_list_free(modified_attachments);
+
+    if (attachment_fd != -1)
+        close(attachment_fd);
+    cut_remove_path(attachment_file, NULL);
+    g_free(attachment_file);
+    cut_remove_path("tmp", NULL);
 }
 
 static void
@@ -105,6 +119,7 @@ run_test_server (const char *data_file_name)
                                    "-m",
                                    data_path,
                                    "--output-message",
+                                   "--color=no",
                                    NULL);
     gcut_process_run(test_server, &error);
     gcut_assert_error(error);
@@ -128,9 +143,43 @@ test_no_attachment (void)
     cut_trace(assert_status("accept"));
 }
 
+static void
+output_attachment_to_file (MzAttachment *attachment)
+{
+    ssize_t written_size;
+
+    attachment_fd = g_mkstemp(attachment_file);
+    written_size = write(attachment_fd, attachment->data, attachment->data_length);
+    cut_assert_equal_int(attachment->data_length, written_size);
+
+    close(attachment_fd);
+    attachment_fd = -1;
+}
+
+static const char *
+get_zip_password (const char *modified_message)
+{
+    char *crlf_pos;
+    char *password;
+
+    password = strstr(modified_message, "X-ZIP-Crypted-Password: ");
+    if (!password)
+        return NULL;
+
+    password += strlen("X-ZIP-Crypted-Password: ");
+    crlf_pos = strchr(password, '\n');
+    if (!crlf_pos)
+        return NULL;
+
+    return cut_take_string(strndup(password, crlf_pos - password));
+}
+
 void
 test_attachment (void)
 {
+    GError *error = NULL;
+    const char *password;
+
     cut_trace(run_test_server("mail"));
     cut_trace(assert_status("pass"));
     cut_assert_match("\\+ X-ZIP-Crypted: Yes", modified_message->str);
@@ -139,5 +188,41 @@ test_attachment (void)
                                                         "=-HY8vxMUek5fQZa/zkovp");
     cut_assert_not_null(modified_attachments);
     cut_assert_equal_int(1, mz_list_length(modified_attachments));
+
+    if (!g_find_program_in_path("unzip"))
+        cut_omit("unzip is not installed in your system!");
+
+    output_attachment_to_file(modified_attachments->data);
+
+    password = get_zip_password(modified_message->str);
+    cut_assert_not_null(password);
+
+    unzip = gcut_process_new("/home/zoe/bin/unzip",
+                             "-o",
+                             "-P", password,
+                             "-d", "tmp",
+                             attachment_file, NULL);
+    cut_assert_true(gcut_process_run(unzip, &error));
+    gcut_assert_error(error);
+
+    cut_assert_equal_int(0, gcut_process_wait(unzip, 10000, &error));
+    gcut_assert_error(error);
+/*
+    cut_assert_exist_path("tmp" G_DIR_SEPARATOR_S "body");
+    expected_file = cut_build_path(cut_get_test_directory(),
+                                   "fixtures",
+                                   "body",
+                                   NULL);
+    cut_assert_equal_file_raw(expected_file,
+                              "tmp" G_DIR_SEPARATOR_S "body");
+
+    cut_assert_exist_path("tmp" G_DIR_SEPARATOR_S "t.png");
+    expected_file = cut_build_path(cut_get_test_directory(),
+                                   "fixtures",
+                                   "t.png",
+                                   NULL);
+    cut_assert_equal_file_raw(expected_file,
+                              "tmp" G_DIR_SEPARATOR_S "t.png");
+*/
 }
 
